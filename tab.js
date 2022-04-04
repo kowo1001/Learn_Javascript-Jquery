a4f7a561237b9ee8fe35bd0decfbf72aae9c1e34
JangJongWook <37354978+kowo1001@users.noreply.github.com>
2022-04-04 18:01:34 +0900

    220404_js&jquery

---
+++ tab.js
@@ -0,0 +1,11 @@
+$(".tab-button").on("click", function () {});
+
+$(".tab-button")
+  .eq(0)
+  .on("click", function () {});
+
+//   버튼0 누르면
+//   - 모든 버튼에 붙은 orange 클래스명 제거
+//   - 버튼0에 orange 클래스명 추가
+//   - 모든 div에 붙은 show 클래스명 제거
+//   - div0에 show 클래스명 추가
